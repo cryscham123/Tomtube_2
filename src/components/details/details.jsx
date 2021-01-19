786562ee0eef2b71f10546a8f3a1e0626b65a4d9
cryscham123 <cryscham123@naver.com>
2021-01-20 08:42:22 +0900

    more sexy description

--- src/components/details/details.jsx
+++ src/components/details/details.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Items from '../items/items';
 import "./details.scss";
 import Youtube from "../../service/youtube";
 
 const Details = ({ history, location: { state } }) => {
     const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+    const desRef = useRef();
+    const btnRef = useRef();
+    let i = 0;
+    const handleClick = () => {
+        if (i === 0) {
+            desRef.current.style.display = "contents";
+            btnRef.current.style.transform = "rotate(180deg)";
+            i = 1;
+        } else if (i === 1) {
+            desRef.current.style.display = "none";
+            btnRef.current.style.transform = "rotate(0deg)";
+            i = 0;
+        }
+    }
     if (state === undefined) {
         history.push("/");
         return null;
@@ -12,16 +26,23 @@ const Details = ({ history, location: { state } }) => {
         return (
             <div className="details">
                 <div className="details__video">
-                    <button className="details__video__des">
-                        <i className="fas fa-chevron-down"></i>
-                    </button>
                     <iframe id="ytplayer" type="text/html" width="720" height="405"
                         src={`https://www.youtube.com/embed/${state.id}`}
-                        frameborder="0" allowfullscreen>
+                        frameBorder="0" allowFullScreen>
                     </iframe>
                     <div className="details__video__info">
                         <h3 className="details__video__info__title">{state.title}</h3>
                         <p className="details__video__info__channel">{state.channel}</p>
+                    </div>
+                    <div className="details__video__des">
+                        <div className="details__video__des__deswrapper">
+                            <p ref={desRef} className="details__video__des__deswrapper__des">
+                                <p className="ulala">{state.des}</p>
+                            </p>
+                        <button ref={btnRef} onClick={handleClick} className="details__video__des__deswrapper__btn">
+                            <i className="fas fa-chevron-down"></i>
+                        </button>
+                        </div>
                     </div>
                 </div>
                 <Items youtube={youtube}/>
